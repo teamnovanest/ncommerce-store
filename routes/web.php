655d3aa6eac455a9f4a52ff5f384ab6a94f18a14
emyawson <46461959+emyawson@users.noreply.github.com>
2021-03-30 14:59:17 +0200

    feat: add auth with customer role

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// All routes that needs to be protected by the customer roles goes inside this function
+
+Route::group(['middleware' => ['role:customer']], function () {
+
+    // Dashboard
+    Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+});
+
+
