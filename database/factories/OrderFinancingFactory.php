60b4bd2dab6357156f27336412bc08d6bb407db0
Andy-Osei <andrew.owusu@novanest.net>
2021-04-07 17:42:49 +0000

    Order Financing added

---
+++ database/factories/OrderFinancingFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\OrderFinancing;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFinancingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = OrderFinancing::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "user_id" => \App\Models\User::all()->random()->id,
+            "lender_organization_id" => \App\Models\Lender::all()->random()->id,
+            "payment_period" => \App\Models\LenderOffering::all()->random()->payment_period,
+        ];
+    }
+}
