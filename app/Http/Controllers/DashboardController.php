db7ad9f35251cba40839a3110aa8d5656f448d96
Andy-Osei <72438783+Andy-Osei@users.noreply.github.com>
2021-04-12 11:43:34 +0000

    User reset password (#19)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\UserFinanceAffiliation;
 use App\Models\CustomerFinanceOrganizationAffiliation;
 
@@ -52,5 +54,43 @@ class DashboardController extends Controller
 
     $insertedId = DB::getPdo()->lastInsertId();
     return response()->json($insertedId);
+    }
+
+    public function changePassword(){
+    return view('auth.change_password');
+    }
+
+    public function updatePassword(Request $request)
+    {
+    $password=Auth::user()->password;
+    $oldpass=$request->oldpass;
+    $newpass=$request->password;
+    $confirm=$request->password_confirmation;
+    if (Hash::check($oldpass,$password)) {
+    if ($newpass === $confirm) {
+    $user=User::find(Auth::id());
+    $user->password=Hash::make($request->password);
+    $user->save();
+    Auth::logout();
+    $notification=array(
+    'messege'=>'Password Changed Successfully ! Now Login with Your New Password',
+    'alert-type'=>'success'
+    );
+    return Redirect()->route('login')->with($notification);
+    }else{
+    $notification=array(
+    'messege'=>'New password and Confirm Password does not match!',
+    'alert-type'=>'error'
+    );
+    return Redirect()->back()->with($notification);
+    }
+    }else{
+    $notification=array(
+    'messege'=>'Old Password does not match!',
+    'alert-type'=>'error'
+    );
+    return Redirect()->back()->with($notification);
+    }
+
     }
 }
