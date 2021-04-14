22ddaa56b66708fc3cff524545766fdda9b97f42
samuelmensah <samuel.mensah@novanest.net>
2021-04-14 12:52:17 +0000

    Feat/ slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,15 +42,14 @@
             </div>
             </div>
             </div>
-                        <!-- </div> -->
-                    <!-- End Left Feature -->
                     <div class="col-lg-9 col-xl-9 col-md-8 col-12">
                         <!-- Start Slider Area -->
                         <div class="slider__container slider--one mrg-xs">
                             <div class="slider__activation__wrap owl-carousel owl-theme">
                                 <!-- Start Single Slide -->
-                                <div class="slide slider__full--screen slider-height-inherit  slider-text-left" style="background:rgba(0, 0, 0, 0) no-repeat scroll center center / cover ;">
-                                    <div class="container">
+                                <div>
+                                    <img src="{{'assets\img\c.jpg'}}" class="slide slider__full--screen slider-height-inherit  slider-text-left" alt="">
+                                    <div class="text-left">
                                         <div class="row">
                                             <div class="col-lg-8 col-xl-8 col-md-12 col-12">
                                                 <div class="slider__inner">
@@ -63,6 +62,25 @@
                                         </div>
                                     </div>
                                 </div>
+                                <!-- End Single Slide -->
+                                <!-- Start Single Slide -->
+                                <div>
+                                    <img src="{{'assets\img\a.jpg'}}" class="slide slider__full--screen slider-height-inherit  slider-text-left" alt="">
+                                    <div class="text-left">
+                                        <div class="row">
+                                            <div class="col-lg-8 col-xl-8 col-md-12 col-12">
+                                                <div class="slider__inner">
+                                                    <h1>New Product <span class="text--theme">Collection</span></h1>
+                                                    <div class="slider__btn">
+                                                        <a class="htc__btn" href="#">shop now</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Single Slide -->
+                               
                             </div>
                         </div>
                         <!-- Start Slider Area -->
