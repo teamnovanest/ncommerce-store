87fefbe718299ec8ea0887b37e2e047d75dcc962
Andy-Osei <andrew.owusu@novanest.net>
2021-05-19 15:06:15 +0000

    feat-404 page

---
+++ resources/views/layouts/error.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>Ncommerce</title>
+ <link rel="stylesheet" href="{{ asset('frontend_new/css/custom.css') }}">
+</head>
+<body>
+ <section class="section-error">
+
+  @yield('content')
+  
+ </section>
+ 
+</body>
+</html>
