7a973be10475abed8a60d66cf4ec7c31a8063515
Emmanuel Kofi <49987349+afariemma@users.noreply.github.com>
2021-06-22 12:58:58 +0000

    bugfix order update (#120)

--- app/Http/Controllers/OrderUpdateController.php
+++ app/Http/Controllers/OrderUpdateController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Auth;
 
 class OrderUpdateController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function updateOrder(Request $request,$orderId,$orderDetailId){
         $status_id = $request->statusId;
         $product_id = $request->productId;
