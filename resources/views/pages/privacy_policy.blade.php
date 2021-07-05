d33879d31bea3f32fef928209e7e8689413e25f9
samuelmensah <72383416+samuelmensah@users.noreply.github.com>
2021-07-05 09:21:59 +0000

    Bugfix- privacy policy (#135)
    * Bugfix- privacy policy

---
+++ resources/views/pages/privacy_policy.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.store')
+
+@section('content')
+
+		 <!-- Start Bradcaump area -->
+        <div class="ht__bradcaump__area">
+            <div class="ht__bradcaump__wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="bradcaump__inner text-center">
+                                <h2 class="bradcaump-title">Privacy Policy</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Bradcaump area -->
+		<!-- cart-main-area start -->
+        <div class="cart-main-area ptb--120 bg__white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-12">
+                    <div>
+                            <div class="policy__section privacy_policy">
+                            <h6 class="policy__updated-date">Updated on 24th May, 2021</h6>
+                            <div class="main-content">
+                            <!-- overview section -->
+                            <h3 class="policy__heading-2">Overview</h3>
+                            <p class="policy__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis harum quidem culpa facilis beatae, blanditiis officia adipisci unde tempore recusandae laboriosam accusamus temporibus at doloribus cumque quasi nostrum distinctio qui maiores magnam ut? Nisi similique temporibus veritatis doloremque consequuntur! Cumque minima tempora ducimus earum dolores omnis provident dignissimos veritatis architecto reiciendis placeat optio assumenda voluptatum nam qui quibusdam amet debitis est, maxime in distinctio. Exercitationem provident quas reprehenderit quia ipsum et dolore neque asperiores qui possimus ad tempora maxime debitis vero, temporibus, nihil totam saepe voluptate! Laborum inventore ad id dolorem! Nobis odio numquam animi nihil soluta aut a officiis.</p>
+
+                            <!-- Consent section -->
+                            <h3 class="policy__heading-2">Collection of Information & Use</h3>
+                            <p class="policy__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis harum quidem culpa facilis beatae, blanditiis officia adipisci unde tempore recusandae laboriosam accusamus temporibus at doloribus cumque quasi nostrum distinctio qui maiores magnam ut? Nisi similique temporibus veritatis doloremque consequuntur! Cumque minima tempora ducimus earum dolores omnis provident dignissimos veritatis architecto reiciendis placeat optio assumenda voluptatum nam qui quibusdam amet debitis est, maxime in distinctio. Exercitationem provident quas reprehenderit quia ipsum et dolore neque asperiores qui possimus ad tempora maxime debitis vero, temporibus, nihil totam saepe voluptate! Laborum inventore ad id dolorem! Nobis odio numquam animi nihil soluta aut a officiis.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+ 
+ @endsection
