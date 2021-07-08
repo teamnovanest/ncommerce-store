b96a6e6d331aaae95315ef7a4cb5ddefa6f6df86
Andy-Osei <andrew.owusu@novanest.net>
2021-07-08 18:26:34 +0000

    fix-meta-data

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,10 +1,15 @@
+@php
+$seo = DB::table('seos')->where('deleted_at', NULL)->first();
+
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <title>Ncommerce</title>
+ <title>{{ $seo->meta_title }}</title>
  <link rel="stylesheet" href="{{ asset('frontend_new/css/custom.css') }}">
 </head>
 <body>
